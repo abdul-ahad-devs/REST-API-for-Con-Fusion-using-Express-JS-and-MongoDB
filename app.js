56ecf99602380486e66a57c17ce8a190d94439f2
dark-theme-programmer <abdulahad.oct99@gmail.com>
2020-04-14 02:21:20 +0500

    Basic Authentication

--- app.js
+++ app.js
@@ -29,6 +29,34 @@ connect.then((db) => {
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+const auth = (req, res, next) => {
+    console.log(req.headers);
+    var authHeader = req.headers.authorization;
+    if(!authHeader) {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err);
+      return;
+    }
+
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+
+    if(user === 'admin' && pass === 'password') {
+      next(); //authorized
+    }
+    else {
+      var err = new Error('You are not Authenticated');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err);
+
+    }
+  }
+
+app.use(auth);
 
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
