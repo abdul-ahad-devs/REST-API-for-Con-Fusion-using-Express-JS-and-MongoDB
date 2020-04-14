dde107cb09ab7119eb77243ca096c9a31f30b375
dark-theme-programmer <abdulahad.oct99@gmail.com>
2020-04-14 15:11:18 +0500

    Cookies

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
@@ -29,8 +28,15 @@ connect.then((db) => {
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser('12345-23451-34512-45123'));
+
 const auth = (req, res, next) => {
-    console.log(req.headers);
+
+  if(!req.signedCookies.user) {
     var authHeader = req.headers.authorization;
     if(!authHeader) {
       var err = new Error('You are not authenticated!');
@@ -39,12 +45,13 @@ const auth = (req, res, next) => {
       next(err);
       return;
     }
-
+  
     var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
     var user = auth[0];
     var pass = auth[1];
-
+  
     if(user === 'admin' && pass === 'password') {
+      res.cookie('user', 'admin', { signed: true })
       next(); //authorized
     }
     else {
@@ -52,23 +59,29 @@ const auth = (req, res, next) => {
       res.setHeader('WWW-Authenticate', 'Basic');
       err.status = 401;
       next(err);
-
+  
     }
   }
+  else {
+    if (req.signedCookies.user === 'admin') {
+        next();
+    }
+    else {
+        var err = new Error('You are not authenticated!');
+        err.status = 401;
+        next(err);
+    }
+  }
+}
 
 app.use(auth);
 
-app.use('/dishes',dishRouter);
-app.use('/promotions',promoRouter);
-app.use('/leaders',leaderRouter);
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/dishes',dishRouter);
+app.use('/promotions',promoRouter);
+app.use('/leaders',leaderRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
