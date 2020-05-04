770ea0bed7fb46fa164f3d2136821aa03b55fca1
dark-theme-programmer <abdulahad.oct99@gmail.com>
2020-05-04 18:02:11 +0500

    Passport Facebook

--- authenticate.js
+++ authenticate.js
@@ -1,6 +1,7 @@
 var passport = require('passport');
 var LocalStratrgy = require('passport-local').Strategy;
 var User = require('./models/user');
+var FacebookTokenStrategy = require('passport-facebook-token');
 
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
@@ -45,6 +46,33 @@ exports.verifyAdmin = (req, res, next) => {
         return next(err);
     }
 }
+
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+    clientID: config.facebook.clientId,
+    clientSecret: config.facebook.clientSecret
+}, (accessToken, refreshToken, profile, done) => {
+    User.findOne({facebookId: profile.id}, (err, user) => {
+        if (err) {
+            return done(err, false);
+        }
+        if (!err && user !== null) {
+            return done(null, user);
+        }
+        else {
+            user = new User({ username: profile.displayName });
+            user.facebookId = profile.id;
+            user.firstname = profile.name.givenName;
+            user.lastname = profile.name.familyName;
+            user.save((err, user) => {
+                if (err)
+                    return done(err, false);
+                else
+                    return done(null, user);
+            })
+        }
+    });
+}
+));
 
 
 passport.use(new LocalStratrgy(User.authenticate()));
