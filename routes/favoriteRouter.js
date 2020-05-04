26ab679204e59bb127b38056c40f63b0ba85032a
dark-theme-programmer <abdulahad.oct99@gmail.com>
2020-05-05 02:01:28 +0500

    Integrating Client and Server

--- routes/favoriteRouter.js
+++ routes/favoriteRouter.js
@@ -106,30 +106,30 @@ favoriteRouter
 
 favoriteRouter
   .route("/:dishId")
-  .get(cors.cors, authenticate.verifyUser, (req, res, next) => {
-    Favorites.findOne({ user: req.user._id })
-      .then(
-        favorites => {
-          if (!favorites) {
+  .get(cors.cors, authenticate.verifyUser, (req,res,next) => {
+    Favorites.findOne({user: req.user._id})
+    .then((favorites) => {
+        if (!favorites) {
             res.statusCode = 200;
-            res.setHeader("Content-Type", "application/json");
-            return res.json({ exists: false, favorites: favorites });
-          } else {
+            res.setHeader('Content-Type', 'application/json');
+            return res.json({"exists": false, "favorites": favorites});
+        }
+        else {
             if (favorites.dishes.indexOf(req.params.dishId) < 0) {
-              res.statusCode = 200;
-              res.setHeader("Content-Type", "application/json");
-              return res.json({ exists: false, favorites: favorites });
-            } else {
-              res.statusCode = 200;
-              res.setHeader("Content-Type", "application/json");
-              return res.json({ exists: true, favorites: favorites });
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                return res.json({"exists": false, "favorites": favorites});
             }
-          }
-        },
-        err => next(err)
-      )
-      .catch(err => next(err));
-  })
+            else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                return res.json({"exists": true, "favorites": favorites});
+            }
+        }
+
+    }, (err) => next(err))
+    .catch((err) => next(err))
+})
   .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
     Favorites.findOne({ user: req.user._id }).then(favorite => {
       if (favorite) {
--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,10 @@ var passport = require('passport');
 var authenticate = require('../authenticate');
 var cors = require('./cors');
 
+
 router.use(bodyParser.json());
+
+router.options('*', cors.corsWithOptions, (req, res) => { res.sendStatus(200); } )
 
 router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, function (req, res, next) {
   User.find({})
@@ -64,12 +67,30 @@ router.post('/signup', cors.corsWithOptions,  (req, res, next) => {
 //   res.json({success: true, status: 'You are successfully logged in!'});
 // });
 
-router.post('/login', cors.corsWithOptions,  passport.authenticate('local'), (req, res) => {
+router.post('/login', cors.corsWithOptions, (req, res, next) => {
 
-  var token = authenticate.getToken({ _id: req.user._id })
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.json({ success: true, token: token, status: 'You are Successful logged in!' });
+  passport.authenticate('local', (err, user, info) => {
+    if (err)
+      return next(err);
+
+    if (!user) {
+      res.statusCode = 401;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({success: false, status: 'Login Unsuccessful!', err: info});
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        res.statusCode = 401;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({success: false, status: 'Login Unsuccessful!', err: 'Could not log in user!'});          
+      }
+
+      var token = authenticate.getToken({_id: req.user._id});
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({success: true, status: 'Login Successful!', token: token});
+    }); 
+  }) (req, res, next);
 });
 
 router.get('/logout', cors.corsWithOptions,  (req, res) => {
@@ -92,6 +113,25 @@ router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res
     res.setHeader('Content-Type', 'application/json');
     res.json({success: true, token: token, status: 'You are successfully logged in!'});
   }
+});
+
+router.get('/checkJWTtoken', cors.corsWithOptions, (req, res) => {
+  passport.authenticate('jwt', {session: false}, (err, user, info) => {
+    if (err)
+      return next(err);
+    
+    if (!user) {
+      res.statusCode = 401;
+      res.setHeader('Content-Type', 'application/json');
+      return res.json({status: 'JWT invalid!', success: false, err: info});
+    }
+    else {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      return res.json({status: 'JWT valid!', success: true, user: user});
+
+    }
+  }) (req, res);
 });
 
 module.exports = router;
